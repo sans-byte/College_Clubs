40571bdcf1638dd3117a2ddb20afe79f7505c0e2
Sanskar Jain <sanskarj15@gmail.com>
2022-02-17 03:01:43 +0530

    added validation to login form (client side)

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,16 +1,34 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 function LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [formError, setFormError] = useState({});
+  const [submit, setSubmit] = useState();
 
   const navigate = useNavigate();
+  const validate = (email, password) => {
+    const errors = {};
+    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    if (!email) {
+      errors.email = "Please enter your email";
+    } else if (!regex.test(email)) {
+      errors.email = "Enter valid email";
+    }
+    if (!password) {
+      errors.password = "Please enter your password";
+    }
+    return errors;
+  };
+  useEffect(() => {
+    setFormError(validate(email, password));
+  }, [email, password]);
 
   const handleLogin = async (e) => {
     e.preventDefault();
-
+    setSubmit(true);
     const res = await fetch("/signin", {
       method: "POST",
       headers: {
@@ -33,68 +51,59 @@ function LoginPage() {
 
   return (
     <div>
-      <div
-        className="hero min-h-screen bg-base-200"
-        style={{
-          backgroundImage: `url(../images/Growth.jpg)`,
-        }}
-      >
-        <div className="flex-col justify-center hero-content lg:flex-row">
-          <div className="text-center lg:text-left">
-            <h1 className="mb-5 text-5xl font-bold text-neutral">Login</h1>
-            <p className="mb-5 text-neutral">
-              Welcome back to space nice to see you again, hope you are doing
-              well...
-            </p>
-          </div>
-          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 bg-opacity-90">
-            <div className="card-body">
-              <form method="POST">
-                <div className="form-control">
-                  <label className="label">
-                    <span className="label-text">Email</span>
-                  </label>
-                  <input
-                    type="text"
-                    placeholder="Email"
-                    className="input input-primary"
-                    value={email}
-                    onChange={(e) => {
-                      e.preventDefault();
-                      setEmail(e.target.value);
-                    }}
-                  />
-                </div>
-                <div className="form-control">
-                  <label className="label">
-                    <span className="label-text">Password</span>
-                  </label>
-                  <input
-                    type="password"
-                    placeholder="Password"
-                    className="input input-primary"
-                    value={password}
-                    onChange={(e) => {
-                      e.preventDefault();
-                      setPassword(e.target.value);
-                    }}
-                  />
-                  <label className="label">
-                    <a href="#" className="label-text-alt">
-                      Forgot password?
-                    </a>
-                  </label>
-                </div>
-                <div className="form-control mt-6">
-                  <input
-                    type="button"
-                    value="Login"
-                    className="btn btn-secondary"
-                    onClick={handleLogin}
-                  />
-                </div>
-              </form>
-            </div>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 bg-opacity-90">
+          <div className="card-body">
+            <form method="POST" onSubmit={handleLogin}>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Email"
+                  className="input input-primary"
+                  value={email}
+                  onChange={(e) => {
+                    e.preventDefault();
+                    setEmail(e.target.value);
+                  }}
+                />
+              </div>
+              <p className="text-center text-red-500">
+                {submit ? formError.email : null}
+              </p>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="Password"
+                  className="input input-primary"
+                  value={password}
+                  onChange={(e) => {
+                    e.preventDefault();
+                    setPassword(e.target.value);
+                  }}
+                />
+                <label className="label">
+                  <a href="#" className="label-text-alt">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <p className="text-center text-red-500">
+                {submit ? formError.password : null}
+              </p>
+              <div className="form-control mt-6">
+                <input
+                  type="submit"
+                  value="Login"
+                  className="btn btn-secondary"
+                />
+              </div>
+            </form>
           </div>
         </div>
       </div>
